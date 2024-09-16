6366c30ba1da1a0243c904c79e3a708389eace97
Lexi Kronowitz <lexikronowitz@lexis-air.wifi.local.cmu.edu>
2024-09-16 16:55:39 -0400

    Added suffix when suggestin new username that has already been taken.

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,7 +131,7 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    showError(username_notify, '[[error:username-taken'+ ' Maybe try ' + username + 'suffix instead]]'); //Got help from teammate Andrew.
+                    showError(username_notify, '[[error:username-taken. Maybe try ' + username + 'suffix instead]]');// Got help from teammate Andrew.
                 }
 
                 callback();
